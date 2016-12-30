ea34b417b4517cf538ea25ddbc327f8cc2bbedc3
PJ Losey <loseyp@gmail.com>
2016-12-30 15:03:26 -0800

    added settings.php for api

---
+++ davidsoncollection/api/settings.php
@@ -0,0 +1,18 @@
+// ---------------------------------------------------------------------------
+// AIRTABLE SETTINGS
+// ---------------------------------------------------------------------------
+
+// Airtable API key.
+// To get your API key, visit: https://airtable.com/api
+define ( 'AIRTABLE_API_KEY', 'keyERzdB2YnyQyRT1' );
+
+// Airtable App ID.
+// To get this value, look at the Authentication notes in the API docs.
+// Example: $ curl https://api.airtable.com/v0/appZZ12rVdg6qzyC/foo...
+// .. where "appZZ12rVdg6qzyC" is the App ID.
+define ( 'AIRTABLE_APP_ID', 'appzQjO5OymxtQjda' );
+
+// Airtable API URL.
+// Default: https://api.airtable.com/v0/
+define ( 'AIRTABLE_API_URL', 'https://api.airtable.com/v0/' );
+  
