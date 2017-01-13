f810b8b6808b0789b8919f4b01e9264825529403
PJ Losey <loseyp@gmail.com>
2017-01-12 21:53:01 -0800

    weather-widget added

---
+++ courses/angular2-1/weather-widget/app/weather-widget/service/weather.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class WeatherService { 
+    getCurrentLocation(): [number,number] {
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(pos => {
+                console.log("Position: ", pos.coords.latitude, "," pos.coords.longitude); // TODO: REMOVE
+                return[pos.coords.latitude, pos.coords.longitude];
+            },
+            err => console.error("Unable to get the position - ", err));
+        } else {
+            console.error("geolocation is not available");
+            return [0,0]
+        }
+    }
+}
